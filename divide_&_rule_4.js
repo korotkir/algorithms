d5ec27d37a7b02fdc3de02e4a59b40bdd72637f9
Kir <ekoroteev27@yandex.ru>
2020-11-19 21:31:31 +0300

    add new function

--- divide_&_rule_4.js
+++ divide_&_rule_4.js
@@ -1,5 +1,32 @@
 // Разделяй и властвуй!
 
+// Вычисление высоты и ширины квадрата из книги (стр 76. "Разделяй и властвуй")
+// Имеется прямоугольник: Ширина = 1680м, высота 640м
+// Требуется равнометрно разделить землю на одинаковые квадратные участки.
+
+function ground(width, height) {
+  if(width === height ) {
+    console.log(((width + height) / 2) / 10)
+  }
+  while(width != height) {
+    if(width > height) {
+      for(i = 0; i < width/height; i++) {
+        width -= height
+      }
+      if(height > width) {
+        for(i = 0; i < height/width; i++) {
+        height -= width
+        }
+      }   
+    }
+    if(width === height) {
+      console.log(`Стороны квадрата равны: ${(width + height) / 2}`)
+    }
+  }
+}
+
+ground(1680, 640)
+
 // Задача:
 // Имеется массив чисел, нужно просуммировать све числа и вернуть сумму
 
