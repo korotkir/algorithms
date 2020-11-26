f1bf4315c1ae85f0dabd4792576acb6fec590a23
Kirill <ekoroteev27@yandex.ru>
2020-11-26 17:25:36 +0300

    add object

--- hash_5.js
+++ hash_5.js
@@ -3,3 +3,17 @@ let book = {}
 book['apple'] = 1.50
 
 console.log(book['apple'])
+
+
+let products = {}
+products['a'] = {}
+products['b'] = {}
+products.a['апельсин'] = 50
+products.a['ананас'] = 150
+products.a['абрикосы'] = 100
+products.b['банан'] = 10
+
+console.table(products)
+console.log(`Банан стоит: ${products.b['банан']}`)
+
+
